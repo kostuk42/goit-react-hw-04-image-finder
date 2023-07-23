7e5782ba276da7c20efe0aa26317c24e4ab26c44
kostiuchenkoo <kostiuchenkoo@upmc.edu>
2023-07-23 11:39:54 +0300

    upd

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { ThreeDots } from 'react-loader-spinner';
 
-class Loader extends React.Component {
-  render() {
-    return (
-      <div className="loader">
-        <ThreeDots className="Loader" type="Oval" color="#3F51B5" height={80} width={80} />
-      </div>
-    );
-  }
+const Loader = () => {
+  return (
+    <div className="loader">
+      <ThreeDots className="Loader" type="Oval" color="#3F51B5" height={80} width={80} />
+    </div>
+  );
 }
 
 export default Loader;
